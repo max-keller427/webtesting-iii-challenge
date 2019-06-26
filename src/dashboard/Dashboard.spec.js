78173a79114bbdff39b8d0961f0b415ad41bbce8
Max Keller <max.keller427@gmail.com>
2019-06-25 20:29:47 -0600

    inital

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
 // Test away
+import React from "react";
+import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
+
+import Dashboard from "./Dashboard";
+
+describe("the dashboard component", () => {
+  it("renders without crashing", () => {
+    const renderTest = render(<Dashboard />);
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,6 @@
 // Test away!
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
+import { render, fireEvent } from "@testing-library/react";
+
+import Display from "./Display";
